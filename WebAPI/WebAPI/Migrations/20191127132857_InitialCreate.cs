866465f10cd676de9c8c3d8da7f9266a3eeb0ee4
Vitor Luis dos Santos <vitor.santos@programmers.com.br>
2019-12-02 11:46:55 -0300

    primeiro commit do projeto

---
+++ WebAPI/WebAPI/Migrations/20191127132857_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails",
+                columns: table => new
+                {
+                    pmId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    cardOwnerName = table.Column<string>(type: "varchar(100)", nullable: false),
+                    cardNumber = table.Column<string>(type: "varchar(16)", nullable: false),
+                    expirationDate = table.Column<string>(type: "varchar(5)", nullable: false),
+                    cvv = table.Column<string>(type: "varchar(3)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails", x => x.pmId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetails");
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Models/PaymentDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int pmId { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string cardOwnerName { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        public string cardNumber { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string expirationDate { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string cvv { get; set; }
+            
+    }
+}
